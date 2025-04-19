886d8dd599afaad64ef5f0604c4fcf1e860c53b5
hvandermillen <hvandermillen@gmail.com>
2025-04-19 01:04:43 -0400

    bible folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import bibleText from './assets/bible.txt?raw'
+import bibleText from './../bible/bible.txt?raw'
 
 async function App() {
   const [count, setCount] = useState(0)
