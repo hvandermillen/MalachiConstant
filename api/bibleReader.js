2506250da36ff71c87a1a7349382139ed30e0f66
hvandermillen <hvandermillen@gmail.com>
2025-04-19 02:21:08 -0400

    bible is in all caps and no spaces

--- api/bibleReader.js
+++ api/bibleReader.js
@@ -1,30 +1,43 @@
 import bibleText from './../bible/bible.txt?raw'
 
-function parseBible () {
+function filterBible () {
     const bibleLines = bibleText.split("\n")
     const bibleLinesText = bibleLines.map(line => {
         const afterColon = line.split(":")
         .slice(1)
-        .join(":")
-        console.log(afterColon)
+        .join(":") //only take text after the colon on each line
         let afterSpace = ""
-        if (afterColon.includes("\t")) {
+        if (afterColon.includes("\t")) { //if tab
             afterSpace = afterColon
-            .split("\t")
+            .split("\t") //take only text after the first tab
             .slice(1)
             .join("\t")
-        } else {
+        } else { //if space
             afterSpace = afterColon
-            .split(" ")
+            .split(" ") //take only text after the first space
             .slice(1)
             .join(" ")
         }
         return afterSpace
     })
-    const newBibleText = bibleLinesText.join("\n")
+    let newBibleText = bibleLinesText.join("\n") //make one string
+    .split("") //array of characters
+    .filter(ch => ((ch.charCodeAt(0)>=65 && ch.charCodeAt(0)<=90) || (ch.charCodeAt(0)>=97 && ch.charCodeAt(0)<=122))) //test if it is a letter
+    .join("") //one string again
+    .toUpperCase()
     return newBibleText
 }
 
+function getDailySymbol() {
+    const bibleString = filterBible() 
+    const currentDay = new Date()
+    const risenDate = new Date("0040-04-09")
+    const daysSinceEaster = (currentDay - risenDate) / (1000 * 60 * 60 * 24)
+    const symbol = `${bibleString.charAt(daysSinceEaster*3)}${bibleString.charAt(daysSinceEaster*3+1)}${bibleString.charAt(daysSinceEaster*3+1)}`  
+    return symbol
+}
+
 export {
-    parseBible
+    filterBible,
+    getDailySymbol
 }
